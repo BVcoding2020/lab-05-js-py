a87177d77165119e043c08c50a2c94010a8ea602
Brielle Veale <63027876+BVcoding2020@users.noreply.github.com>
2020-10-14 15:18:00 -0400

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+	var noun = input("Give me a noun..")
+		var adjective = input("Can i have an adjective?")
+		var adverb = input("May i have an adverb please?")
+		var verb = input("Could i get a verb?")
+
+		print("On a tropical island " + noun + ",")
+		print("Underneth a " + adjective + "lava moon.")
+		print("Hangin' " + adverb + "with the hula dancers,")
+		print(verb + " questions cause' they all got the answers.")
